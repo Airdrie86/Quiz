5302b4b30f08ffed984c6f56be5f569e2740604e
Airdrie86 <gibby86@hotmail.com>
2021-09-10 16:34:59 +0000

    add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
+let startButton = document.getElementById("start-btn");
+let choiceButton = document.getElementById("choice-box")
+let sportButton = document.getElementById("sport");
+let geographyButton = document.getElementById("geography");
+let generalKnowledgeButton = document.getElementById("general-knowledge");
+let questionBox = document.getElementById("question-box");
+let welcome = document.getElementById("welcome");
+let questions = document.getElementById("questions")
+
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    startButton.classList.add("hide")
+    welcome.classList.add("hide")
+    choiceButton.classList.remove("hide")
+}
+
+sportButton.addEventListener("click", sportGame)
+
+function sportGame() {
+    questionBox.classList.remove("hide")
+    choiceButton.classList.add("hide") 
+}
+
+let answerOne = document.getElementById("answer-one")
+
+answerOne.addEventListener('click', this.OnEvent.bind("btn"));
+
+function checkSportAnswer() {
+    if(answerOne.innerText === "2003") {
+        alert("ksjdf")
+    }
+}
     
-})
